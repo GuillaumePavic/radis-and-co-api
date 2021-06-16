ab7663a46f791ff05f03ab3c310419b2dedc77c2
GuillaumePavic <guillaume.pavic.56@gmail.com>
2021-06-16 18:39:27 +0200

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
+const cors = require('cors'); 
 const express = require('express');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 require('./app/routers/index')(app);
